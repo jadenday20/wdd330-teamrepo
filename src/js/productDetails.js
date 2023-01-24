5b9d6f81fff4f8c67ef2634188907cf9e4b9fb61
JaredMalan7 <jared.malan7@gmail.com>
2023-01-23 21:27:28 -0700

    more updates

--- src/js/productDetails.js
+++ src/js/productDetails.js
@@ -1,7 +1,21 @@
 import { setLocalStorage} from "./utils.mjs";
 
-function ProductDetailsTemplate(product){
-    return "<section class='product-detail'> <h3>${product.Brand.Name}</h3><h2 class='divider'>${product.NameWithoutBrand}</h2><img class='divider' src='${product.Image}'alt='${product.NameWithoutBrand}'/> <p class='product-card__price'>$${product.FinalPrice}</p><p class='product__color'>${product.Colors[0].ColorName}</p><p class='product__description'>${product.DescriptionHtmlSimple}</p><div class='product-detail__add'><button id='addToCart' data-id='${product.Id}'>Add to Cart</button></div></section>";
+function productDetailsTemplate(product) {
+  return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
+    <h2 class="divider">${product.NameWithoutBrand}</h2>
+    <img
+      class="divider"
+      src="${product.Image}"
+      alt="${product.NameWithoutBrand}"
+    />
+    <p class="product-card__price">$${product.FinalPrice}</p>
+    <p class="product__color">${product.Colors[0].ColorName}</p>
+    <p class="product__description">
+    ${product.DescriptionHtmlSimple}
+    </p>
+    <div class="product-detail__add">
+      <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+    </div></section>`;
 }
 
 export default class ProductDetails {
@@ -31,10 +45,12 @@ export default class ProductDetails {
         //   }
         setLocalStorage("so-cart", this.product);
       }
-      renderProductDetails(selector){
+      renderProductDetails(selector) {
         const element = document.querySelector(selector);
         element.insertAdjacentHTML(
-            "afterBegin", ProductDetailsTemplate(this.product));
-        
-      }
+          "afterBegin",
+          productDetailsTemplate(this.product)
+        );
+        }
+
 }
