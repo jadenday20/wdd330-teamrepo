ab99d8c3e5c5c1125055d03152ee5463b6362868
JaredMalan7 <jared.malan7@gmail.com>
2023-02-01 20:42:50 -0700

    updates activity 3

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,17 +1,18 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
-    return `<li class="product-card">
-    <a href="product_pages/index.html?product=">
-    <img
-      src=""
-      alt="Image of "
-    />
-    <h3 class="card__brand"></h3>
-    <h2 class="card__name"></h2>
-    <p class="product-card__price">$</p></a>
-  </li>`
-}   
+  return `<li class="product-card">
+  <a href="product_pages/index.html?product=${product.Id}">
+  <img
+    src="${product.Image}"
+    alt="Image of ${product.Name}"
+  />
+  <h3 class="card__brand">${product.Brand.Name}</h3>
+  <h2 class="card__name">${product.Name}</h2>
+  <p class="product-card__price">$${product.FinalPrice}</p></a>
+</li>`;
+}
+
 
 export default class ProductList {
     constructor(category, dataSource, listElement) {
@@ -25,5 +26,11 @@ export default class ProductList {
       // our dataSource will return a Promise...so we can use await to resolve it.
       const list = await this.dataSource.getData();
       // render the list 
+      this.renderList(list);
     }
+    
+  renderList(list) {
+    renderListWithTemplate(productCardTemplate, this.listElement, list);
+  }
+
   }
