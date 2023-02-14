71feb9d3aa94a29b5452be3899ca190f85be8513
Jared Malan <jared.malan7@gmail.com>
2023-02-14 10:01:23 -0700

    main js fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-
+import { loadHeaderFooter } from "./utils.mjs";
+loadHeaderFooter ();
 
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
