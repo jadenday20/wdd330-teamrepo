8e03f45fa21da69e3a68c1a91f477146c47a3bba
jadenday20 <day18007@byui.edu>
2023-02-14 11:21:59 -0700

    added the tent backpack, sleeping bag, and hammock buttons

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 import ProductData from "./ProductData.mjs";
-import ProductList from "./productList.mjs";
+import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 loadHeaderFooter ();
 
