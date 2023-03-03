e9506ce0e40716b591ea698ae5bb5384fe0ac2e8
jadenday20 <day18007@byui.edu>
2023-03-03 09:01:08 -0700

    got the checkout working

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -1,40 +1,192 @@
-export function calculateSubtotal(productList){
-    var subtotal = 0;
-    for (let i = 0; i < productList.length; i++){
-        subtotal += productList[i].FinalPrice;
-    }
-    return subtotal;
+// export function calculateSubtotal(productList){
+//     var subtotal = 0;
+//     for (let i = 0; i < productList.length; i++){
+//         subtotal += productList[i].FinalPrice;
+//     }
+//     return subtotal;
+// }
+
+// export function calculateTaxes(subtotal){
+//     var taxes = (subtotal * 0.06).toFixed(2); 
+//     return taxes;
+// }
+
+// export function calculateShipping(numItems){
+//     var shipping = 10 + numItems * 2;
+//     return shipping;
+// }
+
+// export function calculateTotal(subtotal, taxes, shipping){
+//     return parseFloat(subtotal) + parseFloat(taxes) + parseFloat(shipping);
+// }
+
+// export function orderSummaryTemplate(numItems, subtotal, taxes, shipping, total) {
+//     // return `<fieldset class="summary-template">
+//     // <legend>Order Summary</legend>
+//     //   <label for="subtotal" class="">Item Subtotal(${numItems}):</label>
+//     //   <p name="subtotal">${subtotal}</p>
+//     //   <label for="shipping" class="">Shipping Estimate:</label>
+//     //   <p name="shipping"> ${shipping}</p>
+//     //   <label for="tax" class="">Tax:</label> 
+//     //   <p name="tax">${taxes}</p>
+//     //   <label for="orderTotal" class=""><b>Order Total:<b/></label> 
+//     //   <p name="orderTotal">${total}</p>
+//     //   </fieldset>`;
+//     document.getElementById("numItems").textContent = numItems;
+//     document.getElementById("cartTotal").textContent = "$" + subtotal;
+//     document.getElementById("shipping").textContent = "$" + shipping;
+//     document.getElementById("tax").textContent = "$" + taxes;
+//     document.getElementById("orderTotal").textContent = "$" + total;
+//   }
+
+import { getLocalStorage } from "./utils.mjs";
+import ExternalServices from "./ExternalServices.mjs";
+
+const services = new ExternalServices();
+function formDataToJSON(formElement) {
+  const formData = new FormData(formElement),
+    convertedJSON = {};
+
+  formData.forEach(function (value, key) {
+    convertedJSON[key] = value;
+  });
+
+  return convertedJSON;
 }
 
-export function calculateTaxes(subtotal){
-    var taxes = (subtotal * 0.06).toFixed(2); 
-    return taxes;
+function packageItems(items) {
+  const simplifiedItems = items.map((item) => {
+    console.log(item);
+    return {
+      id: item.Id,
+      price: item.FinalPrice,
+      name: item.Name,
+      quantity: 1,
+    };
+  });
+  return simplifiedItems;
 }
+// function packageDetails(details) {
+//   const simplifiedDetails = details.map((item) => {
+//     console.log(item);
+//     return {
+//       quantity: 1,
+//       quantity: 1,
+//       quantity: 1,
+//       quantity: 1,
+//       quantity: 1,
+//       quantity: 1,
+//       quantity: 1,
+//       quantity: 1,
+//       quantity: 1,
+//       quantity: 1,
+//     };
+//   });
+//   return simplifiedDetails;
+// }
 
-export function calculateShipping(numItems){
-    var shipping = 10 + numItems * 2;
-    return shipping;
-}
-
-export function calculateTotal(subtotal, taxes, shipping){
-    return parseFloat(subtotal) + parseFloat(taxes) + parseFloat(shipping);
-}
-
-export function orderSummaryTemplate(numItems, subtotal, taxes, shipping, total) {
-    // return `<fieldset class="summary-template">
-    // <legend>Order Summary</legend>
-    //   <label for="subtotal" class="">Item Subtotal(${numItems}):</label>
-    //   <p name="subtotal">${subtotal}</p>
-    //   <label for="shipping" class="">Shipping Estimate:</label>
-    //   <p name="shipping"> ${shipping}</p>
-    //   <label for="tax" class="">Tax:</label> 
-    //   <p name="tax">${taxes}</p>
-    //   <label for="total" class=""><b>Order Total:<b/></label> 
-    //   <p name="total">${total}</p>
-    //   </fieldset>`;
-    document.getElementById("numItems").textContent = numItems;
-    document.getElementById("cart-subtotal").textContent = "$" + subtotal;
-    document.getElementById("shipping-est").textContent = "$" + shipping;
-    document.getElementById("Tax").textContent = "$" + taxes;
-    document.getElementById("total").textContent = "$" + total;
+export default class CheckoutProcess {
+  constructor(key, outputSelector) {
+    this.key = key;
+    this.outputSelector = outputSelector;
+    this.list = [];
+    this.itemTotal = 0;
+    this.shipping = 0;
+    this.tax = 0;
+    this.orderTotal = 0;
   }
+  init() {
+    this.list = getLocalStorage(this.key);
+    this.calculateItemSummary();
+  }
+  calculateItemSummary() {
+    const summaryElement = document.querySelector(
+      this.outputSelector + " #cartTotal"
+    );
+    const itemNumElement = document.querySelector(
+      this.outputSelector + " #numItems"
+    );
+    itemNumElement.innerText = this.list.length;
+    // calculate the total of all the items in the cart
+    const amounts = this.list.map((item) => item.FinalPrice);
+    this.itemTotal = amounts.reduce((sum, item) => sum + item);
+    summaryElement.innerText = "$" + this.itemTotal;
+  }
+  calculateOrdertotal() {
+    this.shipping = 10 + (this.list.length - 1) * 2;
+    this.tax = (this.itemTotal * 0.06).toFixed(2);
+    this.orderTotal = (
+      parseFloat(this.itemTotal) +
+      parseFloat(this.shipping) +
+      parseFloat(this.tax)
+    ).toFixed(2);
+    this.displayOrderTotals();
+  }
+  displayOrderTotals() {
+    const shipping = document.querySelector(this.outputSelector + " #shipping");
+    const tax = document.querySelector(this.outputSelector + " #tax");
+    const orderTotal = document.querySelector(
+      this.outputSelector + " #orderTotal"
+    );
+    shipping.innerText = "$" + this.shipping;
+    tax.innerText = "$" + this.tax;
+    orderTotal.innerText = "$" + this.orderTotal;
+  }
+  // getHTMLElements() {
+  //   this.fname = document.querySelector(
+  //     "#firstName"
+  //   ).value;
+  //   this.lname = document.querySelector(
+  //     "#lastName"
+  //   ).value;
+  //   this.street = document.querySelector(
+  //     "#street"
+  //   ).value;
+  //   this.city = document.querySelector(
+  //     "#city"
+  //   ).value;
+  //   this.state = document.querySelector(
+  //     "#state"
+  //   ).value;
+  //   this.zip = document.querySelector(
+  //     "#zip"
+  //   ).value;
+  //   this.cardNumber = document.querySelector(
+  //     "#cardNumber"
+  //   ).value;
+  //   this.expiration = document.querySelector(
+  //     "#expiration"
+  //   ).value;
+  //   this.code = document.querySelector(
+  //     "#security"
+  //   ).value;
+  // }
+  async checkout() {
+    const formElement = document.forms["checkout"];
+
+    const json = formDataToJSON(formElement);
+    // add totals, and item details
+    json.orderDate = new Date().toJSON();
+    json.orderTotal = this.orderTotal;
+    json.tax = this.tax;
+    json.shipping = this.shipping;
+    json.items = packageItems(this.list);
+    // this.getHTMLElements();
+    json.fname = document.querySelector("#firstName").value;
+    json.lname = document.querySelector("#lastName").value;
+    json.street = document.querySelector("#street").value;
+    json.city = document.querySelector("#city").value;
+    json.state = document.querySelector("#state").value;
+    json.zip = document.querySelector("#zip").value;
+    json.cardNumber = document.querySelector("#cardNumber").value;
+    json.expiration = document.querySelector("#expiration").value;
+    json.code = document.querySelector("#security").value;
+    console.log(json);
+    try {
+      const res = await services.checkout(json);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
