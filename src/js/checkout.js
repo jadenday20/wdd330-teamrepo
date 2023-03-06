a90d1274d8cacb115feb07169b97d7c14499b540
Jared Malan <jared.malan7@gmail.com>
2023-03-06 16:40:10 -0700

    changes that we may not use

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,20 +1,21 @@
-import {
-  orderSummaryTemplate,
-  calculateSubtotal,
-  calculateTaxes,
-  calculateShipping,
-  calculateTotal,
-} from "./CheckoutProcess.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
-import { getLocalStorage } from "./utils.mjs";
+import CheckoutProcess from "./CheckoutProcess.mjs";
 
 loadHeaderFooter();
 
-let cartContents = getLocalStorage("so-cart");
+const myCheckout = new CheckoutProcess("so-cart", ".checkout-summary");
+myCheckout.init();
 
-let numItems = cartContents.length;
-let subtotal = calculateSubtotal(cartContents);
-let taxes = calculateTaxes(subtotal);
-let shipping = calculateShipping(numItems);
-let total = calculateTotal(subtotal, taxes, shipping);
-orderSummaryTemplate(numItems, subtotal, taxes, shipping, total);
+document
+  .querySelector("#zip")
+  .addEventListener("blur", myCheckout.calculateOrdertotal.bind(myCheckout));
+// listening for click on the button
+document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
+  e.preventDefault();
+  // const myForm = document.forms[0];
+  // const chk_status = myForm.checkValidity();
+  // myForm.reportValidity();
+  // if(chk_status) 
+
+  myCheckout.checkout();
+});
